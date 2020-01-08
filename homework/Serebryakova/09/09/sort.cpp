55b355fa92bb1597abd4a0abf5b13d50f6e584da
Zay <zay082008@gmail.com>
2020-01-08 19:05:20 +0300

    fix hw 09

--- homework/Serebryakova/09/09/sort.cpp
+++ homework/Serebryakova/09/09/sort.cpp
@@ -160,11 +160,11 @@ int main(){
     
     try {
         file_sort(out);
+        
     } catch(const std::runtime_error &err) {
         std::cerr << err.what() << std::endl;
         return 1;
     }
     return 0;
 }
-
 
